beceb9eacc7caaeab84a5e4da5b68e8ba2855309
Francesc M <francescmm@gmail.com>
2020-06-25 08:33:47 +0200

    Modifying header files license

--- BenchmarkLib/TimeProvider.h
+++ BenchmarkLib/TimeProvider.h
@@ -1,36 +1,47 @@
 #pragma once
 
-/****************************************************************************************
- ** BenchmarkTool is a library to register benchmarks of a process.
- **
- ** LinkedIn: www.linkedin.com/in/cescmm/
- ** Web: www.francescmm.com
- **
- ** This lbirary is free software; you can redistribute it and/or
- ** modify it under the terms of the GNU Lesser General Public
- ** License as published by the Free Software Foundation; either
- ** version 2 of the License, or (at your option) any later version.
- **
- ** This library is distributed in the hope that it will be useful,
- ** but WITHOUT ANY WARRANTY; without even the implied warranty of
- ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- ** Lesser General Public License for more details.
- **
- ** You should have received a copy of the GNU Lesser General Public
- ** License along with this library; if not, write to the Free Software
- ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- ***************************************************************************************/
+/****************************************************************************
+**
+** Copyright (C) 2020 Francesc Martinez
+** LinkedIn: www.linkedin.com/in/cescmm/
+** Web: www.francescmm.com
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**     * Redistributions of source code must retain the above copyright
+**       notice, this list of conditions and the following disclaimer.
+**     * Redistributions in binary form must reproduce the above copyright
+**       notice, this list of conditions and the following disclaimer in the
+**       documentation and/or other materials provided with the distribution.
+**     * Neither the name of Francesc M. nor the
+**       names of its contributors may be used to endorse or promote products
+**       derived from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+** DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+** ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+****************************************************************************/
 
 #include <ITimeProvider.h>
 
-namespace GitQlientTools {
+namespace GitQlientTools
+{
 
 class TimeProvider : public ITimeProvider
 {
 public:
-    TimeProvider() = default;
+   TimeProvider() = default;
 
-    std::chrono::microseconds getTimeSinceEpochMsecs() const override;
+   std::chrono::microseconds getTimeSinceEpochMsecs() const override;
 };
 
 }
